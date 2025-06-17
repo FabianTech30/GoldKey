6e7394e55ed15726d7790b85fd0ce9c0132aec6a
FabianTech30 <fabybaldenegro@gmail.com>
2025-06-17 03:09:58 -0700

    valores de Route atributos hechos

--- rutas-back/src/main/java/gm/rutasback/model/Route.java
+++ rutas-back/src/main/java/gm/rutasback/model/Route.java
@@ -1,5 +1,31 @@
 package gm.rutasback.model;
 
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "routes")
 public class Route {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 15)
+    private String name;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RouteType type;
+
+    @Column(nullable = false)
+    private Integer capacity;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id", nullable = false)
+    private City city;
+
+    @ManyToOne
+    @JoinColumn(name = "driver_id", nullable = false)
+    private Employee driver;
+    
+
 }
