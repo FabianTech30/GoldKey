4d6ece90553f5d6f26022773faacfd83618b5d94
FabianTech30 <fabybaldenegro@gmail.com>
2025-06-17 03:46:32 -0700

    se agrega capa de service

---
+++ rutas-back/src/main/java/gm/rutasback/service/CityService.java
@@ -0,0 +1,15 @@
+package gm.rutasback.service;
+
+import com.cormex.model.City;
+import com.cormex.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface CityService {
+    @Autowired
+    private CityRepository cityRepository;
+
+    public List
+
+}
