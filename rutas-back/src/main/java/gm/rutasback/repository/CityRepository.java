0e2c01a755b7121d900bbf335d2027702c406bb0
FabianTech30 <fabybaldenegro@gmail.com>
2025-06-17 03:23:31 -0700

    se crean ciudad y empleado repositorio

---
+++ rutas-back/src/main/java/gm/rutasback/repository/CityRepository.java
@@ -0,0 +1,9 @@
+package gm.rutasback.repository;
+
+import gm.rutasback.model.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Long> {
+}
---
+++ rutas-back/src/main/java/gm/rutasback/repository/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package gm.rutasback.repository;
+
+import gm.rutasback.model.City;
+import gm.rutasback.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> findByCityAndActiveTrue(City city);
+    boolean existsByIdAndRoutesNotEmpty(Long employeeId);
+}
