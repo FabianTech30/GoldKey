d0c591dd8e0b1bebc6d45d7b8db0d8560b1313a3
FabianTech30 <fabybaldenegro@gmail.com>
2025-06-17 02:35:07 -0700

    primer commit

---
+++ rutas-back/src/main/java/gm/rutasback/RutasBackApplication.java
@@ -0,0 +1,13 @@
+package gm.rutasback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutasBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RutasBackApplication.class, args);
+    }
+
+}
---
+++ rutas-back/src/test/java/gm/rutasback/RutasBackApplicationTests.java
@@ -0,0 +1,13 @@
+package gm.rutasback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutasBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
