4c46cc73c2ee6d1244da1cea48b246de2a53c388
FabianTech30 <fabybaldenegro@gmail.com>
2025-06-17 02:51:29 -0700

    se agregan valores de ciudad

--- rutas-back/src/main/java/gm/rutasback/model/City.java
+++ rutas-back/src/main/java/gm/rutasback/model/City.java
@@ -1,5 +1,33 @@
 package gm.rutasback.model;
 
+import jakarta.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(names = "cities")
 public class City {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @OneToMany(mappedBy = "city")
+    private Set<Employee> employees;
+
+    @OneToMany(mappedBy = "city")
+    private Set<Route> routes;
+
+    //CONSTRUCTORS, GETTERS, SETTERS
+    publico City() {}
+
+    public City(String name) {
+        this.name = name;
+    }
+
+    //GETTERS AND SETTERS
+    public Long
+
+
 }
