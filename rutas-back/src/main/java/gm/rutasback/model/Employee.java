3fa5203e65928720a07243bb5403a54abe3282b0
FabianTech30 <fabybaldenegro@gmail.com>
2025-06-17 02:59:48 -0700

    valores de atributo empleado agregados

--- rutas-back/src/main/java/gm/rutasback/model/Employee.java
+++ rutas-back/src/main/java/gm/rutasback/model/Employee.java
@@ -2,6 +2,8 @@ package gm.rutasback.model;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "employees")
 public class Employee {
@@ -15,5 +17,22 @@ public class Employee {
     @Column(nullable = false, length = 15)
     private String lastName;
 
-    
+    @Column(length = 15)
+    private String motherLastName;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @Column(nullable = false)
+    private double salary;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id", nullable = false)
+    private City city;
+
+    @one
+
 }
