18da7fc504dd827cb4c097bca0f31d1ae49a19c5
FabianTech30 <fabybaldenegro@gmail.com>
2025-06-18 03:41:19 -0700

    se agregar DTO de rutas y se asignan validaciones

---
+++ rutas-back/src/main/java/gm/rutasback/dto/RouteDTO.java
@@ -0,0 +1,29 @@
+package gm.rutasback.dto;
+
+import gm.rutasback.model.RouteType;
+import jakarta.validation.constraints.*;
+
+public class RouteDTO {
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    @Size(max = 15, message = "Name cannot exceed 15 characters")
+    @Pattern(regexp = "^[a-zA-Z0-9 ]+$", message = "Name can only contain alphanumeric characters")
+    private String name;
+
+    @NotNull(message = "Type is required")
+    private RouteType type;
+
+    @NotNull(message = "Capacity is required")
+    @Positive(message = "Capacity must be positive")
+    private Integer capacity;
+
+    @NotNull(message = "City ID is required")
+    private Long cityId;
+
+    @NotNull(message = "Driver ID is required")
+    private Long driverId;
+
+    //Getters and Setters
+
+}
