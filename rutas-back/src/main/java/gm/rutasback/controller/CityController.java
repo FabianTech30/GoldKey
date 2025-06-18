5eff97f78cdfece26358b604958f04449b60ab33
FabianTech30 <fabybaldenegro@gmail.com>
2025-06-18 07:37:06 -0700

    Se agrega controlador de ciudad

---
+++ rutas-back/src/main/java/gm/rutasback/controller/CityController.java
@@ -0,0 +1,34 @@
+package gm.rutasback.controller;
+
+
+import gm.rutasback.model.City;
+import gm.rutasback.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cities")
+public class CityController {
+    @Autowired
+    private CityService cityService;
+
+    @GetMapping
+    public List<City>getAllCities() {
+        return cityService.getAllCities();
+    }
+
+    @GetMapping("/{id}")
+    public City getCityById(@PathVariable Long id) {
+        return cityService.getCityById(id);
+    }
+
+
+
+
+
+}
