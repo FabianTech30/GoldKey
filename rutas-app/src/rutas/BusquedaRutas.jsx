df2cce0aa45abb035d6679fc8cec6de40b248f6e
FabianTech30 <fabybaldenegro@gmail.com>
2025-06-19 05:03:21 -0700

    se agregar busqeda por rutas

--- rutas-app/src/rutas/BusquedaRutas.jsx
+++ rutas-app/src/rutas/BusquedaRutas.jsx
@@ -13,52 +13,65 @@ export default function BusquedaRutas() {
             <h1>Busqueda de Rutas</h1>
         </div>
          <div className=' container dark:bg-gray-800text-justify text-1xl p-2 border-4 border-gray-500' style={{marginTop: '20px'}} >
-            <h3 className='p-2'>Ciudad</h3>
+            <h3 className='p-2'>CIUDAD</h3>
             <ComboBox 
             options={cities}
             label=""
             />
-            <h3>Rutas por Ciudad</h3>
+            <div className='flex justify-between items-center' style={{marginTop: '20px'}}>
+              <h3 className='text-3xl'>RUTAS POR CIUDAD</h3>
+              <a href="/" className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' style={{marginTop: '20px'}}>
+                    Agregar Ruta
+              </a>
+            </div>
             
-            
-
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg" style={{marginTop: '20px'}}>
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" class="px-6 py-3">
-                            Product name
+                            ID ruta
                         </th>
                         <th scope="col" class="px-6 py-3">
-                            Color
+                            Nombre Ruta
                         </th>
                         <th scope="col" class="px-6 py-3">
-                            Category
+                            Tipo
                         </th>
                         <th scope="col" class="px-6 py-3">
-                            Price
+                            Capacidad
                         </th>
                         <th scope="col" class="px-6 py-3">
-                            Action
+                            Modificar
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Eliminar
                         </th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple MacBook Pro 17"
+                            1
                         </th>
                         <td class="px-6 py-4">
-                            Silver
+                            Ruta Culiacan 2
                         </td>
                         <td class="px-6 py-4">
-                            Laptop
+                            Articulo
                         </td>
                         <td class="px-6 py-4">
-                            $2999
+                            15
                         </td>
                         <td class="px-6 py-4">
-                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            <button className='hover:bg-blue-700 text-white font-bold' style={{marginTop: '20px'}}>
+                              Editar
+                            </button>
+                        </td>
+                        <td class="px-6 py-4">
+                            <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded' style={{marginTop: '20px', marginLeft: '10px'}}>
+                              Eliminar
+                            </button>
                         </td>
                     </tr>
                 </tbody>
